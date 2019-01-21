8dfce0b1ec8a87c76d5a3e5798b347cbc42070ec
Muhammad Qasim Riaz <qasim.riaz002@gmail.com>
2019-01-22 01:35:33 +0500

    Addition in "LearnGUI_5" File: Added Tutorial For Performing Operation Using Event Without Defining Function

--- LearnGUI_5_BindingFunctionsToGuiLayoutAndEvents.py
+++ LearnGUI_5_BindingFunctionsToGuiLayoutAndEvents.py
@@ -20,8 +20,13 @@ button1.pack()
 button2 = Button(root, text = "Call Function Using Event")
 button2.pack()
 
+button3 = Button(root, text = "Performing Operation Using Event Without Defining Function")
+button3.pack()
+
 button2.bind("<Button-1>", funcLeftClick) #On Clicking the Left Button Mouse
 button2.bind("<Button-2>", funcScrollClick) #On Clicking the Scroll Button on Mouse
-button2.bind("<Button-3>", funcRigthClick) #On Clickong the Rigth Button on Mouse
+button2.bind("<Button-3>", funcRigthClick) #On Clicking the Rigth Button on Mouse
+
+button3.bind("<Button-1>", print("Performing Operation Using Event Without Defining Function")) #On Clicking the Rigth Button on Mouse
 
 root.mainloop()
